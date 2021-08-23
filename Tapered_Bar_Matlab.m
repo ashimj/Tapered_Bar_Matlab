f388439681b64ba53b6b66c05503e289cee9e8d7
Ashim Joshi <60934810+theashimj1@users.noreply.github.com>
2021-08-23 18:36:40 +0545

    Add files via upload

---
+++ Tapered_Bar_Matlab.m
@@ -0,0 +1,35 @@
+clear all
+clc
+%clg
+axis normal
+% Tapering steel
+E = 200E9; % Pa
+A0 = (pi/4)*(50E-3)^2; % m^2
+At = (pi/4)*(20E-3)^2; % m^2
+L = 0.1; % m
+echo on
+N = 10; % Number of elements
+echo off
+% Compute element length, area, k and force
+Le = L/N;
+for i = 1:N,
+Atop = A0 -(A0-At)/N*(i-1);
+Abot = A0 - (A0-At)/N*i;
+A(i) = (Atop+Abot)/2;
+k(i) = A(i)*E/Le;
+end
+% Assembly of the stiffness matrix using k's.
+K = zeros(N,N);
+K(1,1) = k(1) + k(2);
+K(1,2) = -k(2);
+for i = 2:N-1,
+K(i,i-1) = -k(i);
+K(i,i) = k(i) + k(i+1);
+K(i,i+1) = -k(i+1);
+end
+K(N,N-1) = -k(N);
+K(N,N) = k(N);
+F=[0;0;0;0;0;0;0;0;0;300];
+% Solve for displacements {q}. It is a column vector.
+q =(inv(K)*F)*1000;
+
